7c064d5c3a1caf00bf83c652760af7152ff7f9d5
satner <cse2343@gmail.com>
2018-08-20 01:08:34 +0300

    Add scroll up button

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import CustomCarousel from '../layout/CustomCarousel';
 import DisplayData from '../layout/DisplayData';
 import SearchBar from '../layout/SearchBar';
-import Footer from '../layout/Footer'
+import Footer from '../layout/Footer';
+import ScrollUpButton from "react-scroll-up-button";
 
 class Home extends Component {
  
@@ -10,6 +11,7 @@ class Home extends Component {
 
     return (
       <div >
+         <ScrollUpButton EasingType='linear'/>
         <CustomCarousel />
         <h1 id="main-header" className="display-4 text-center" >Active Experiences</h1>
         
